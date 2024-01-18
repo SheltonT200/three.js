ebc6bb0ed3a37098df60c5ebae89ab2015710c54
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-01-18 14:23:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.SphereGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const s = new THREE.Mesh( geometry, material );
+scene.add( s );
 
 camera.position.z = 5;
 
